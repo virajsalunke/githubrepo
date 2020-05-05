cd4240809f285539cc8d0ecc25d3b27c78f52ad9
Viraj Salunke <virajsalunke@users.noreply.github.com>
2020-05-05 11:45:37 +0530

    Jenkins Git Eclipse v1.2

--- gitHub/src/gitHub/Sample.java
+++ gitHub/src/gitHub/Sample.java
@@ -3,7 +3,7 @@ package gitHub;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java Github and Jenkins v1.1");
+		System.out.println("Hello Java Github and Jenkins v1.2");
 
 	}
 
